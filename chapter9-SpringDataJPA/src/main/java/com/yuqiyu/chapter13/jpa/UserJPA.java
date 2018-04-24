553ac065619a082723f64c05a43403721759f4e1
yushi19791201 <yushiwh@126.com>
2018-04-24 18:42:34 +0800

    SpringBoot的DataJPA操作，包含分页查询

---
+++ chapter9-SpringDataJPA/src/main/java/com/yuqiyu/chapter13/jpa/UserJPA.java
@@ -0,0 +1,40 @@
+package com.yuqiyu.chapter13.jpa;
+
+import com.yuqiyu.chapter13.base.BaseRepository;
+import com.yuqiyu.chapter13.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：恒宇少年
+ * Date：2017/4/14
+ * Time：23:08
+ * 码云：http://git.oschina.net/jnyqy
+ * ========================
+ */
+@Transactional
+public interface UserJPA extends BaseRepository<User, Long> {
+    /**
+     * 查询大于20岁的用户
+     */
+    @Query(value = "select * from user where age > ?1", nativeQuery = true)
+    public List<User> nativeQuery(int age);
+
+
+    /**
+     * //根据用户名、密码删除一条数据
+     *
+     * @param name
+     * @param password
+     */
+    @Modifying
+    @Query(value = "delete from user where name = ?1 and password = ?2", nativeQuery = true)
+    public void deleteQuery(String name, String password);
+}
