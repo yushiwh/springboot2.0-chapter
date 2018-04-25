d891388fa1152e9cdec72664035e1e1b4cee97c2
yushi19791201 <yushiwh@126.com>
2018-04-25 14:55:47 +0800

    修改一下

--- chapter18-oauth2/src/main/java/com/yuqiyu/chapter18/entity/Authority.java
+++ chapter18-oauth2/src/main/java/com/yuqiyu/chapter18/entity/Authority.java
@@ -23,12 +23,18 @@ public class Authority {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Authority authority = (Authority) o;
 
-        if (!name.equals(authority.name)) return false;
+        if (!name.equals(authority.name)) {
+            return false;
+        }
 
         return true;
     }
