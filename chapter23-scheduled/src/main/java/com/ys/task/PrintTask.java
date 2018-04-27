bf82350d968315836f09557f10116653bcd1f8fe
yushi19791201 <yushiwh@126.com>
2018-04-27 11:23:24 +0800

    SpringBoot的定时任务

--- chapter23-scheduled/src/main/java/com/ys/task/PrintTask.java
+++ chapter23-scheduled/src/main/java/com/ys/task/PrintTask.java
@@ -21,40 +21,36 @@ public class PrintTask {
     /**
      * 每小时的10分执行该方法
      */
-   // @Scheduled(cron = "0 10 * * * *")
-    public void cron() throws Exception
-    {
-        System.out.println("执行测试cron时间："+ new Date(System.currentTimeMillis()));
+    // @Scheduled(cron = "0 10 * * * *")
+    public void cron() throws Exception {
+        System.out.println("执行测试cron时间：" + new Date(System.currentTimeMillis()));
     }
 
 
     /**
-     * 是上一个调用开始后再次调用的延时（不用等待上一次调用完成）
+     * 上一个调用开始后再次调用的延时（不用等待上一次调用完成），这样就会存在重复执行的问题，所以不是建议使用，但数据量如果不大时在配置的间隔时间内可以执行完也是可以使用的
      */
     //@Scheduled(fixedRate = 1000 * 1)
-    public void fixedRate() throws Exception
-    {
+    public void fixedRate() throws Exception {
         Thread.sleep(2000);
-        System.out.println("执行测试fixedRate时间："+ new Date(System.currentTimeMillis()));
+        System.out.println("执行测试fixedRate时间：" + new Date(System.currentTimeMillis()));
     }
 
     /**
      * 上一个调用完成后再次调用的延时调用
      */
     //@Scheduled(fixedDelay = 1000 * 1)
-    public void fixedDelay() throws Exception
-    {
+    public void fixedDelay() throws Exception {
         Thread.sleep(3000);
-        System.out.println("执行测试fixedDelay时间："+ new Date(System.currentTimeMillis()));
+        System.out.println("执行测试fixedDelay时间：" + new Date(System.currentTimeMillis()));
     }
 
     /**
      * 第一次被调用前的延时，单位毫秒
      */
-  //  @Scheduled(initialDelay = 1000 * 10,fixedDelay = 1000 * 2)
-    public void initialDelay() throws Exception
-    {
-        System.out.println("执行测试initialDelay时间："+ new Date(System.currentTimeMillis()));
+    //  @Scheduled(initialDelay = 1000 * 10,fixedDelay = 1000 * 2)
+    public void initialDelay() throws Exception {
+        System.out.println("执行测试initialDelay时间：" + new Date(System.currentTimeMillis()));
     }
 
 }
