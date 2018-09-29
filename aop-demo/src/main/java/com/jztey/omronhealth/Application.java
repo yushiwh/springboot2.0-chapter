1f33b01e1b718d982d6ad2915f56ce6dee64538d
yushi19791201 <yushiwh@126.com>
2018-09-29 08:33:00 +0800

    SpringBoot的AOP例子，5种通知

--- aop-demo/src/main/java/com/jztey/omronhealth/Application.java
+++ aop-demo/src/main/java/com/jztey/omronhealth/Application.java
@@ -1,14 +1,9 @@
 package com.jztey.omronhealth;
 
 
-import com.jztey.omronhealth.service.ArithmeticCalculator;
-
-import com.jztey.omronhealth.service.ArithmeticCalculatorImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 
 /**
--- aop-demo/src/main/java/com/jztey/omronhealth/service/LoggingAspect.java
+++ aop-demo/src/main/java/com/jztey/omronhealth/service/LoggingAspect.java
@@ -4,9 +4,8 @@ import java.util.Arrays;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,58 +15,132 @@ import org.springframework.stereotype.Component;
  * com.springsource.org.aopalliance-1.0.0.jar
  * com.springsource.org.aspectj.weaver-1.6.8.RELEASE.jar
  * spring-aspects-4.0.0.RELEASE.jar
- *
+ * <p>
  * 2. 在 Spring 的配置文件中加入 aop 的命名空间。
- *
+ * <p>
  * 3. 基于注解的方式来使用 AOP
  * 3.1 在配置文件中配置自动扫描的包: <context:component-scan base-package="com.atguigu.spring.aop"></context:component-scan>
  * 3.2 加入使 AspjectJ 注解起作用的配置: <aop:aspectj-autoproxy></aop:aspectj-autoproxy>
  * 为匹配的类自动生成动态代理对象.
- *
+ * <p>
  * 4. 编写切面类:
  * 4.1 一个一般的 Java 类
  * 4.2 在其中添加要额外实现的功能.
- *
+ * <p>
  * 5. 配置切面
  * 5.1 切面必须是 IOC 中的 bean: 实际添加了 @Component 注解
  * 5.2 声明是一个切面: 添加 @Aspect
  * 5.3 声明通知: 即额外加入功能对应的方法.
  * 5.3.1 前置通知: @Before("execution(public int com.atguigu.spring.aop.ArithmeticCalculator.*(int, int))")
+ *
  * @Before 表示在目标方法执行之前执行 @Before 标记的方法的方法体.
  * @Before 里面的是切入点表达式:
- *
+ * <p>
  * 6. 在通知中访问连接细节: 可以在通知方法中添加 JoinPoint 类型的参数, 从中可以访问到方法的签名和方法的参数.
- *
+ * <p>
  * 7. @After 表示后置通知: 在方法执行之后执行的代码.
  */
 
 //通过添加 @Aspect 注解声明一个 bean 是一个切面!
+
+/**
+ * 可以使用 @Order 注解指定切面的优先级, 值越小优先级越高
+ */
+@Order(2)
 @Aspect
 @Component
-//@Slf4j
+@Slf4j
 public class LoggingAspect {
 
-	//声明该方法执行之前执行，前置通知
-	@Before("execution(public int com.jztey.omronhealth.service.ArithmeticCalculator.*(int, int))")
-	public void beforeMethod(JoinPoint joinPoint){
-		String methodName = joinPoint.getSignature().getName();
-		Object [] args = joinPoint.getArgs();
+    /**
+     * 定义一个方法, 用于声明切入点表达式. 一般地, 该方法中再不需要添入其他的代码.
+     * 使用 @Pointcut 来声明切入点表达式.
+     * 后面的其他通知直接使用方法名来引用当前的切入点表达式.
+     * （..）表示任意参数
+     */
+    @Pointcut("execution(public int com.jztey.omronhealth.service.ArithmeticCalculator.*(..))")
+    public void declareJointPointExpression() {
+    }
 
-		System.out.println("这是切面开始打印出来的--->The method " + methodName + " begins with " + Arrays.asList(args));
+
+    //声明该方法执行之前执行，前置通知
+    //直接导入切面点，上面的
+    @Before("declareJointPointExpression()")
+    public void beforeMethod(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        System.out.println("这是切面开始打印出来的--->The method " + methodName + " begins with " + Arrays.asList(args));
+    }
+
+    //后置通知：在目标方法执行后（无论是否发生异常），执行通知
+    //在后置通知中还不能访问目标方法的执行的结果，不是在执行方法后调用的
+
+    /**
+     * 这是切面开始打印出来的--->The method add begins with [3, 5]
+     * 这是切面结束打印出来的--->The method add ends
+     * 和--->8
+     */
+    @After("declareJointPointExpression()")
+    public void afterMethod(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("这是切面结束打印出来的--->The method " + methodName + " ends");
+    }
+
+
+    /**
+     * 带有返回值的切面
+     * 在方法法正常结束受执行的代码
+     * 返回通知是可以访问到方法的返回值的!
+     * 可以使用returning = "result"进行获取后得到
+     */
+    @AfterReturning(value = "declareJointPointExpression()",
+            returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("The method " + methodName + " ends with " + result);
+    }
+
+    /**
+     * 异常处理切面
+     * 在目标方法出现异常时会执行的代码.
+     * 可以访问到异常对象; 且可以指定在出现特定异常时在执行通知代码
+     */
+    @AfterThrowing(value = "declareJointPointExpression()",
+            throwing = "e")
+    public void afterThrowing(JoinPoint joinPoint, Exception e) {
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("The method " + methodName + " occurs excetion:" + e);
+    }
+/**
+ * 环绕切面，一般用的不是很多，类似于动态代理，可以包含前面4种的任意个
+ * 环绕通知需要携带 ProceedingJoinPoint 类型的参数.
+ * 环绕通知类似于动态代理的全过程: ProceedingJoinPoint 类型的参数可以决定是否执行目标方法.
+ * 且环绕通知必须有返回值, 返回值即为目标方法的返回值
+ */
+	/*
+	@Around("execution(public int com.atguigu.spring.aop.ArithmeticCalculator.*(..))")
+	public Object aroundMethod(ProceedingJoinPoint pjd){
+
+		Object result = null;
+		String methodName = pjd.getSignature().getName();
+
+		try {
+			//前置通知
+			System.out.println("The method " + methodName + " begins with " + Arrays.asList(pjd.getArgs()));
+			//执行目标方法
+			result = pjd.proceed();
+			//返回通知
+			System.out.println("The method " + methodName + " ends with " + result);
+		} catch (Throwable e) {
+			//异常通知
+			System.out.println("The method " + methodName + " occurs exception:" + e);
+			throw new RuntimeException(e);
+		}
+		//后置通知
+		System.out.println("The method " + methodName + " ends");
+
+		return result;
 	}
-
-	//后置通知：在目标方法执行后（无论是否发生异常），执行通知
-	//在后置通知中还不能访问目标方法的执行的结果，不是在执行方法后调用的
-	/**
-	 * 这是切面开始打印出来的--->The method add begins with [3, 5]
-	 * 这是切面结束打印出来的--->The method add ends
-	 * 和--->8
-	 *
-	 */
-	@After("execution(* com.jztey.omronhealth.service.*.*(..))")
-	public void afterMethod(JoinPoint joinPoint){
-		String methodName = joinPoint.getSignature().getName();
-		System.out.println("这是切面结束打印出来的--->The method " + methodName + " ends");
-	}
-
+	*/
 }
