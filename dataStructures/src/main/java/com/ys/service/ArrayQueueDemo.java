7eb2be4a9d754d123410e14a5fa53a01fcd5b6d5
yushi <yushiwh@126.com>
2019-06-24 15:40:51 +0800

    增加数组模拟队列

---
+++ dataStructures/src/main/java/com/ys/service/ArrayQueueDemo.java
@@ -0,0 +1,186 @@
+package com.ys.service;
+
+
+import java.util.Scanner;
+
+/**
+ * 数组模拟队列
+ */
+public class ArrayQueueDemo {
+
+    public static void main(String[] args) {
+        //测试一把
+        //创建一个队列
+        ArrayQueue queue = new ArrayQueue(3);
+        //接收用户输入
+        char key = ' ';
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列头的数据");
+            //接收一个字符
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                //取出数据
+                case 'g':
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                //查看队列头的数据
+                case 'h':
+                    try {
+                        int res = queue.headQueue();
+                        System.out.printf("队列头的数据是%d\n", res);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                //退出
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        System.out.println("程序退出~~");
+    }
+
+}
+
+/**
+ * 使用数组模拟队列-编写一个ArrayQueue类
+ */
+
+class ArrayQueue {
+    /**
+     * // 表示数组的最大容量
+     */
+    private int maxSize;
+    /**
+     * // 队列头
+     */
+    private int front;
+    /**
+     * // 队列尾
+     */
+    private int rear;
+    /**
+     * // 该数据用于存放数据, 模拟队列
+     */
+    private int[] arr;
+
+    /**
+     * // 创建队列的构造器
+     *
+     * @param arrMaxSize
+     */
+
+    public ArrayQueue(int arrMaxSize) {
+        maxSize = arrMaxSize;
+        arr = new int[maxSize];
+        /**
+         * // 指向队列头部，分析出front是指向队列头的前一个位置.
+         */
+        front = -1;
+        /**
+         * // 指向队列尾，指向队列尾的数据(即就是队列最后一个数据)
+         */
+        rear = -1;
+    }
+
+    /**
+     * 判断队列是否满
+     */
+    public boolean isFull() {
+        return rear == maxSize - 1;
+    }
+
+
+    /**
+     * 判断队列是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+
+    /**
+     * 添加数据到队列
+     *
+     * @param n
+     */
+    public void addQueue(int n) {
+        // 判断队列是否满
+        if (isFull()) {
+            System.out.println("队列满，不能加入数据~");
+            return;
+        }
+        // 让rear 后移
+        rear++;
+        arr[rear] = n;
+    }
+
+    /**
+     * 获取队列的数据, 出队列
+     */
+
+    public int getQueue() {
+        // 判断队列是否空
+        if (isEmpty()) {
+            // 通过抛出异常
+            throw new RuntimeException("队列空，不能取数据");
+        }
+        front++; // front后移
+        return arr[front];
+
+    }
+
+    /**
+     * 显示队列的所有数据
+     */
+    public void showQueue() {
+        // 遍历
+        if (isEmpty()) {
+            System.out.println("队列空的，没有数据~~");
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        }
+    }
+
+    /**
+     * 显示队列的头数据， 注意不是取出数据
+     */
+    public int headQueue() {
+        // 判断
+        if (isEmpty()) {
+            throw new RuntimeException("队列空的，没有数据~~");
+        }
+        return arr[front + 1];
+    }
+}
---
+++ dataStructures/src/test/java/com/ys/ArrayQueueDemoTest.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2018-2020, 998电商集团
+ * FileName: ArrayQueueDemoTest
+ * Author:   yushi
+ * Date:     2019/6/24 14:59
+ * Description: 测试数组模拟队列
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.ys;
+
+
+/**
+ * 〈测试数组模拟队列〉
+ *
+ * @author yushi
+ * @create 2019/6/24
+ * @since 1.0.0
+ */
+public class ArrayQueueDemoTest extends Tester {
+
+
+}
