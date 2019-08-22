a0e9f3e8dbff9f62d9c29f8001e06a023a4699bb
yushi <yushiwh@126.com>
2019-08-22 17:31:28 +0800

    增加选择排序

---
+++ dataStructures/src/main/java/com/ys/service/sort/InsertSort.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (C), 2018-2020, 998电商集团
+ * FileName: InsertSort
+ * Author:   nick
+ * Date:     2019/8/22 11:27
+ * Description: 插入排序
+ * History:
+ */
+package com.ys.service.sort;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 〈插入排序〉
+ *
+ * @author nick
+ * @create 2019/8/22
+ * @since 1.0.0
+ */
+@Component
+public class InsertSort {
+    //插入排序
+    public void insertSort(int[] arr) {
+        int insertVal = 0;
+        int insertIndex = 0;
+        //使用for循环来把代码简化
+        for (int i = 1; i < arr.length; i++) {
+            //定义待插入的数
+            insertVal = arr[i];
+            // 即arr[1]的前面这个数的下标
+            insertIndex = i - 1;
+
+            // 给insertVal 找到插入的位置
+            // 说明
+            // 1. insertIndex >= 0 保证在给insertVal 找插入位置，不越界
+            // 2. insertVal < arr[insertIndex] 待插入的数，还没有找到插入位置
+            // 3. 就需要将 arr[insertIndex] 后移
+            while ( insertIndex >= 0 && insertVal < arr[insertIndex] ) {
+                // arr[insertIndex]
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex--;
+            }
+            // 当退出while循环时，说明插入的位置找到, insertIndex + 1
+            // 举例：理解不了，我们一会 debug
+            //这里我们判断是否需要赋值
+            if (insertIndex + 1 != i) {
+                arr[insertIndex + 1] = insertVal;
+            }
+
+            //System.out.println("第"+i+"轮插入");
+            //System.out.println(Arrays.toString(arr));
+        }
+
+
+		/*
+
+
+		//使用逐步推导的方式来讲解，便利理解
+		//第1轮 {101, 34, 119, 1};  => {34, 101, 119, 1}
+
+
+		//{101, 34, 119, 1}; => {101,101,119,1}
+		//定义待插入的数
+		int insertVal = arr[1];
+		int insertIndex = 1 - 1; //即arr[1]的前面这个数的下标
+
+		//给insertVal 找到插入的位置
+		//说明
+		//1. insertIndex >= 0 保证在给insertVal 找插入位置，不越界
+		//2. insertVal < arr[insertIndex] 待插入的数，还没有找到插入位置
+		//3. 就需要将 arr[insertIndex] 后移
+		while(insertIndex >= 0 && insertVal < arr[insertIndex] ) {
+			arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+			insertIndex--;
+		}
+		//当退出while循环时，说明插入的位置找到, insertIndex + 1
+		//举例：理解不了，我们一会 debug
+		arr[insertIndex + 1] = insertVal;
+
+		System.out.println("第1轮插入");
+		System.out.println(Arrays.toString(arr));
+
+		//第2轮
+		insertVal = arr[2];
+		insertIndex = 2 - 1;
+
+		while(insertIndex >= 0 && insertVal < arr[insertIndex] ) {
+			arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+			insertIndex--;
+		}
+
+		arr[insertIndex + 1] = insertVal;
+		System.out.println("第2轮插入");
+		System.out.println(Arrays.toString(arr));
+
+
+		//第3轮
+		insertVal = arr[3];
+		insertIndex = 3 - 1;
+
+		while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+			arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+			insertIndex--;
+		}
+
+		arr[insertIndex + 1] = insertVal;
+		System.out.println("第3轮插入");
+		System.out.println(Arrays.toString(arr)); */
+
+    }
+}
---
+++ dataStructures/src/test/java/com/ys/sort/InsertSortTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C), 2018-2020, 998电商集团
+ * FileName: sparsearray
+ * Author:   yushi
+ * Date:     2019/6/20 14:59
+ * Description: 稀疏数组测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.ys.sort;
+
+import com.ys.Tester;
+import com.ys.service.sort.InsertSort;
+import com.ys.service.sort.SelectSort;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 插入排序的测试
+ *
+ * @author yushi
+ * @create 2019/8/22
+ * @since 1.0.0
+ */
+public class InsertSortTest extends Tester {
+
+    @Autowired
+    private InsertSort insertSort;
+
+    @Test
+    public void testStack() throws Exception {
+
+        //int[] arr = {101, 34, 119, 1, -1, 89};
+        // 创建要给80000个的随机的数组
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            // 生成一个[0, 8000000) 数
+            arr[i] = (int) (Math.random() * 8000000);
+        }
+
+        System.out.println("插入排序前");
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        //调用插入排序算法
+        insertSort.insertSort(arr);
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序前的时间是=" + date2Str);
+
+        //System.out.println(Arrays.toString(arr));
+    }
+
+
+}
