a312d609c7922ded200b8870e459c4d9b1a88a51
yushi <yushiwh@126.com>
2019-09-04 14:31:44 +0800

    增加归并排序

--- dataStructures/src/test/java/com/ys/sort/MergetSortTest.java
+++ dataStructures/src/test/java/com/ys/sort/MergetSortTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 
@@ -31,28 +32,28 @@ public class MergetSortTest extends Tester {
 
     @Test
     public void testStack() throws Exception {
-        //int arr[] = { 8, 4, 5, 7, 1, 3, 6, 2 }; //
+        int arr[] = { 8, 4, 5, 7, 1, 3, 6, 2 }; //
 
         //测试快排的执行速度
         // 创建要给80000个的随机的数组
-        int[] arr = new int[80000000];
-        for (int i = 0; i < 80000000; i++) {
-            arr[i] = (int) (Math.random() * 80000000); // 生成一个[0, 8000000) 数
-        }
-        System.out.println("排序前");
-        Date data1 = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1Str = simpleDateFormat.format(data1);
-        System.out.println("排序前的时间是=" + date1Str);
-
+//        int[] arr = new int[80000000];
+//        for (int i = 0; i < 80000000; i++) {
+//            arr[i] = (int) (Math.random() * 80000000); // 生成一个[0, 8000000) 数
+//        }
+//        System.out.println("排序前");
+//        Date data1 = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date1Str = simpleDateFormat.format(data1);
+//        System.out.println("排序前的时间是=" + date1Str);
+//
         int temp[] = new int[arr.length]; //归并排序需要一个额外空间
         mergetSort.mergeSort(arr, 0, arr.length - 1, temp);
+//
+//        Date data2 = new Date();
+//        String date2Str = simpleDateFormat.format(data2);
+//        System.out.println("排序前的时间是=" + date2Str);
 
-        Date data2 = new Date();
-        String date2Str = simpleDateFormat.format(data2);
-        System.out.println("排序前的时间是=" + date2Str);
-
-        //System.out.println("归并排序后=" + Arrays.toString(arr));
+        System.out.println("归并排序后=" + Arrays.toString(arr));
 
     }
 
