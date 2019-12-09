6f56e82e41186e8dc1be8a574868d0c1a3bbdf70
nick <yushiwh@126.com>
2019-12-09 10:57:44 +0800

    增加AVL树的左旋转、右旋转、双旋转

--- dataStructures/src/main/java/com/ys/service/avl/Node.java
+++ dataStructures/src/main/java/com/ys/service/avl/Node.java
@@ -176,7 +176,6 @@ public class Node {
                 // 递归的向右子树添加
                 this.right.add(node);
             }
-
         }
         //当添加完一个结点后，如果: (右子树的高度-左子树的高度) > 1 , 左旋转
         if (rightHeight() - leftHeight() > 1) {
